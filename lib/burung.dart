c008b718c2e0073439c62229a889f0025709f09a
Hendri Gunawan <hendrigunawan368@gmail.com>
2024-03-04 21:43:23 +0700

    Mendelete project weather app dan isinya

--- lib/burung.dart
+++
@@ -1,9 +0,0 @@
-// import 'inheritance.dart';
-//
-//
-// class Burung extends Animal {
-//   final String furcolor;
-//
-//   Burung(String name, int age, int weight, this.furcolor)
-//       : super(name, age, weight);
-// }
